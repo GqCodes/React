086c73096df66c123e8e6cb402e1b98141e270df
Chris Vargas <gqcodes@gmail.com>
2023-01-25 09:54:43 -0500

    working with state & click events

---
+++ myapp-dg/src/Content.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const Content = () => {
+  const [name, setName] = useState('Chris');
+  const [count, setCount] = useState(0);
+
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Chris'];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int]);
+  };
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked!`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
+  return (
+    <main>
+      <p onDoubleClick={handleClick}>Hello {name}</p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
+    </main>
+  );
+};
+
+export default Content;
