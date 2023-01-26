54452480e6c395b338873cc544b1dae7d4d68adf
Chris Vargas <gqcodes@gmail.com>
2023-01-26 10:37:40 -0500

    worked with lists, keys, props, prop-drilling

--- myapp-dg/src/Content.js
+++ myapp-dg/src/Content.js
@@ -1,34 +1,13 @@
-import { useState } from 'react';
+import ItemList from './ItemList';
 
-const Content = () => {
-  const [name, setName] = useState('Chris');
-  const [count, setCount] = useState(0);
-
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Chris'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  };
-
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  };
-
-  const handleClick2 = (name) => {
-    console.log(`${name} was clicked!`);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  };
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}</p>
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleClick}>Click it</button>
-      <button onClick={(e) => handleClick3(e)}>Click it</button>
+      {items.length ? (
+        <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
+      ) : (
+        <p style={{ marginTop: '2rem' }}>This list is empty!</p>
+      )}
     </main>
   );
 };
--- myapp-dg/src/Header.js
+++ myapp-dg/src/Header.js
@@ -1,9 +1,15 @@
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
 
+Header.defaultProps = {
+  title: 'Default Title',
+};
+
 export default Header;
+
+/* Default Props: All us to set values for the props expected in the components. If not provided, then default values will take over.  */
