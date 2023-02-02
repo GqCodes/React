0d972f109fda00a4cfd4131f452c912263b5a208
Chris Vargas <gqcodes@gmail.com>
2023-02-02 16:26:24 -0500

    final part

--- react-nn/myapp/src/Home.js
+++ react-nn/myapp/src/Home.js
@@ -1,36 +1,60 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+import Create from './Create';
+import useFetch from './useFetch';
 
 const Home = () => {
-  // State
+  // const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+
+  /* useState */
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
   const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
 
-  // Click Events
-  const handleClick = () => {
-    setName('Luigi');
-    setAge(30);
-  };
-  const handleClickAgain = (name, e) => {
-    console.log(`Hello ${name}`, e.target);
-  };
+  /* useEffect - use a empty dependency array [] so that the hook's function only runs after the first initial render, if the state changes, it wont run the function again. or expliciately include a specific dependnecy like {name} for example where as useEffect runs ONLY when the name state is updated/changed */
 
-  // Styles
-  const buttonStyle = {
-    marginLeft: '5px',
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('Could not fecth data');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
   };
 
   return (
     <div className='home'>
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
 
-      <button onClick={handleClick}>Click Me</button>
+      {blogs && <BlogList blogs={blogs} title='All Blogs' />}
 
-      <button style={buttonStyle} onClick={(e) => handleClickAgain('Chris', e)}>
-        Click Me Again
-      </button>
+      <button onClick={() => setName('Luigi')}>Change name</button>
+      <p>{name}</p>
+
+      {/* Filters the blogs by author */}
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />
+
+      <Create />
     </div>
   );
 };
